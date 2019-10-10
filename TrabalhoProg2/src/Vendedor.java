702f7dd009ba13569900ed9417b96dfea4c4c20f
Lui Fhelipe <lf.93@outlook.com>
2019-10-10 04:10:41 -0400

    Vendedor v1.3

--- TrabalhoProg2/src/Vendedor.java
+++ TrabalhoProg2/src/Vendedor.java
@@ -2,44 +2,66 @@
 
 public class Vendedor extends Funcionario{
 
-	  private int vendas;
-	  private int meta;
+	  private Vendas[] vendas;
+	  private double meta;
 	  
 	
 	public Vendedor(String nome, String cpf, Endereco endereco, String telefone, double salario) {
 		super(nome, cpf, endereco, telefone, salario);
 		
-		this.setVendas(0);
+//		this.setVendas(0);
 	}
 
 
-	public int getVendas() {
-		return vendas;
+	public void addVendas(Vendas novaVenda) {
+		if(isFull(vendas))
+			redimensionaVetor(vendas);
+		for (int i = 0; i < vendas.length; i++)
+			if(vendas[i] == null)
+				vendas[i] = novaVenda;
 	}
 
 
-	public void setVendas(int vendas) {
-		this.vendas = vendas;
-	}
-
-	public boolean realizaVenda() {
-		
-		this.vendas++;
-		
-		if(this.vendas == meta) {
-	
-			this.setSalario(this.getSalario() + this.getSalario() * 0.10);
+	public double valorTotalVendas(Vendas[] vendas) {
+		double valorTotalVendas = 0;
+		for (int i = 0; i < vendas.length; i++) {
+			valorTotalVendas += vendas[i].calcularVendas();
 		}
-		return true;
+		return valorTotalVendas;
 	}
+//  ??????????????????????????
+//	public boolean realizaVenda() {
+//		
+//		this.vendas++;
+//		
+//		if(this.vendas == meta) {
+//	
+//			this.setSalario(this.getSalario() + this.getSalario() * 0.10);
+//		}
+//		return true;
+//	}
 
 
-	public int getMeta() {
+	public double getMeta() {
 		return meta;
 	}
 
 
 	public void setMeta(int meta) {
 		this.meta = meta;
+	}
+	
+	public boolean isFull(Vendas[] vetor) {
+		for (int i = 0; i < vetor.length; i++) 
+			if(vetor[i] == null)
+				return false;
+		return true;
+	}
+	
+	public void redimensionaVetor(Vendas[] vetor) {
+		Vendas[] aux = new Vendas[vetor.length + 1];
+		for (int i = 0; i < aux.length; i++)
+			aux[i] = vetor[i];
+		vetor = aux;
 	}
 }
