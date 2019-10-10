485f7f899c1167cd5875d3caef92853ad4a500e0
FelipeRochaTI <55464799+FelipeRochaTI@users.noreply.github.com>
2019-10-10 05:10:42 -0400

    Add files via upload
    v1.4

---
+++ TrabalhoProg2/src/TrabalhoProg2/src/Gerente.java
@@ -0,0 +1,13 @@
+
+
+public class Gerente extends Funcionario {
+
+	public Gerente(String nome, String cpf, Endereco endereco, String telefone, double salario) {
+		super(nome, cpf, endereco, telefone, salario);
+		this.setSalario(this.getSalario()+ this.getSalario() * 0.20);
+	}
+	public void imprimeRelatorio() {
+		super.imprimeRelatorio();
+		System.out.println("Bonificação: "+ (super.getSalario()*0.20));
+	}
+}
---
+++ TrabalhoProg2/src/TrabalhoProg2/src/Padeiro.java
@@ -0,0 +1,28 @@
+
+
+public class Padeiro extends Funcionario{
+	double horasNoturnas;
+	
+	public Padeiro(String nome, String cpf, Endereco endereco, String telefone, double salario) {
+		super(nome, cpf, endereco, telefone, salario);
+		this.horasNoturnas = 0;
+	}
+	
+	public boolean jornadaNoturna() {
+		this.setSalario(this.getSalario()+(this.getSalario() * 0.25) * horasNoturnas);
+		return true;
+	}
+
+	public double getHorasNoturnas() {
+		return horasNoturnas;
+	}
+
+	public void setHorasNoturnas(double horasNoturnas) {
+		this.horasNoturnas = horasNoturnas;
+	}
+
+	public void imprimeRelatorio() {
+		super.imprimeRelatorio();
+		System.out.println("Bonificação: "+ ((super.getSalario()*0.25)*horasNoturnas));
+	}
+}
